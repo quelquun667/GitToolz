0d8b2f06502278d9d30107326d18f5e7aa40352b
quelquun667 <87375238+quelquun667@users.noreply.github.com>
2025-07-15 21:26:06 +0000

    Faire en sorte que la zone quand on switch sur Raw soit de la taille que

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,16 +1,34 @@
 import * as React from 'react';
 
 import {cn} from '@/lib/utils';
+import { useIsomorphicLayoutEffect } from '@/hooks/use-isomorphic-layout-effect';
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'textarea'>>(
   ({className, ...props}, ref) => {
+    const internalRef = React.useRef<HTMLTextAreaElement>(null);
+    React.useImperativeHandle(ref, () => internalRef.current!);
+
+    const [isMounted, setIsMounted] = React.useState(false);
+
+    useIsomorphicLayoutEffect(() => {
+      setIsMounted(true);
+    }, []);
+
+    useIsomorphicLayoutEffect(() => {
+      const textarea = internalRef.current;
+      if (textarea && isMounted) {
+        textarea.style.height = 'auto';
+        textarea.style.height = `${textarea.scrollHeight}px`;
+      }
+    }, [isMounted, props.value]);
+
     return (
       <textarea
+        ref={internalRef}
         className={cn(
           'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
           className
         )}
-        ref={ref}
         {...props}
       />
     );

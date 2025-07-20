b3edef407d25c63f6d7ac560caadca15cdf4663f
CMoiNoah <noahgranizo@gmail.com>
2025-07-20 12:15:10 +0000

    J'ai ça au résultat du graphe...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
-  title: 'GitToolz',
+  title: 'GitDocs',
   description: 'Generate documentation, changelogs, and more from your Git repository.',
 };
 
--- src/next.config.ts
+++ src/next.config.ts
@@ -16,6 +16,10 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+       {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      }
     ],
   },
   devIndicators: {

0cee6937d47ac9d084e508903570535bd73d3a86
quelquun667 <87375238+quelquun667@users.noreply.github.com>
2025-07-20 12:51:34 +0000

    Ça ne le génére juste pas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
-  title: 'GitDocs',
+  title: 'GitToolz',
   description: 'Generate documentation, changelogs, and more from your Git repository.',
 };
 
